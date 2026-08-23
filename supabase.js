91a4adca09e433a58f7691d7987899f89aaebdf3
insticts1 <destinobioha1@gmail.com>
2026-08-23 15:46:21 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL = "https://kkqgatzlgkkqzqcdstce.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://kkqgatzlgkkqzqcdstce.supabase.co";
 const SUPABASE_KEY = "sb_publishable_77zLC47j_QLCJgjsaSVq7A_eyc7Mk0K";
 
 const supabaseClient = supabase.createClient(
-    SUPABASE_URL, = https://kkqgatzlgkkqzqcdstce.supabase.co/rest/v1/
-    SUPABASE_KEY = sb_publishable_77zLC47j_QLCJgjsaSVq7A_eyc7Mk0K
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
